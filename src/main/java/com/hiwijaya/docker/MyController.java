ae518a8ac96e0469bef03a9f5cadb595232ea007
Happy Indra Wijaya <dev.hiwijaya@gmail.com>
2021-10-05 14:18:47 +0700

    set env variable externally

--- src/main/java/com/hiwijaya/docker/MyController.java
+++ src/main/java/com/hiwijaya/docker/MyController.java
@@ -1,8 +1,10 @@
 package com.hiwijaya.docker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 
 /**
  * @author Happy Indra Wijaya
@@ -10,10 +12,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MyController {
 
+    @Value("${app.name}")
+    private String name;
+
+    @Value("${app.message}")
+    private String message;
+
     @GetMapping("/")
     @ResponseBody
     public String index(){
-        return "<h1>[JAVA-DOCKER]<h1/>";
+        return "<h1>[JAVA-DOCKER]</h1>" + "<b>Hello, " + name + "</b><p>" + message + "</p>";
     }
 
 }
